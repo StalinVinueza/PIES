c743fc186c69f549ca8401e8e92599a6419726fd
StalinVinueza <csvinueza@puce.edu.ec>
2025-02-01 22:53:53 -0500

    coreccion server

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-const poolPostgres = require('./dbconfig/dbconection.js');
-
 // Carpeta pública para el frontend
 app.use(express.static('client'));
 
