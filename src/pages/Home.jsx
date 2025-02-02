d417d09cb667a6d9024818f38597d60cbbce7358
StalinVinueza <csvinueza@puce.edu.ec>
2025-02-02 12:17:28 -0500

    home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Bienvenido a Eco-Shop</h1>
+      <p>Explora nuestros productos, conoce nuestra historia y mucho más.</p>
+    </div>
+  );
+}
+
+export default Home;
