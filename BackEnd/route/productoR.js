dde84d5f8cd2f0a8819fb7b4b4ea9124d91ce46c
StalinVinueza <csvinueza@puce.edu.ec>
2025-02-06 22:33:43 -0500

    productos crud postman

---
+++ BackEnd/route/productoR.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const productoController = require('../controller/productoC');
+const upload = require('../middleware/upload'); 
+
+// Rutas para productos
+router.get('/productos', productoController.getAllProductos);
+router.get('/productos/:id', productoController.getProductoById);
+router.post('/productos', upload.single('es_pro_imagen'), productoController.createProducto);
+router.put('/productos/:id', upload.single('es_pro_imagen'), productoController.updateProducto);
+router.delete('/productos/:id', productoController.deleteProducto);
+
+module.exports = router;
