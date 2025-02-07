8d8d5f08acfc675ac153e5f40139d0b056c603d1
Stalin Vinueza <csvinueza@puce.edu.ec>
2025-02-06 19:03:03 -0500

    orden 2

--- BackEnd/middleware/upload.js
+++ BackEnd/middleware/upload.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Configuración del almacenamiento
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./uploads");
+    cb(null, "BackEnd/uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}${path.extname(file.originalname)}`);
