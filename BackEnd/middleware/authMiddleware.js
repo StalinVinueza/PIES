bff928c9277d73d2b0880de46bd45dc3a323b48b
StalinVinueza <csvinueza@puce.edu.ec>
2025-02-08 22:02:44 -0500

    vistas menu

--- BackEnd/middleware/authMiddleware.js
+++ BackEnd/middleware/authMiddleware.js
@@ -1,45 +1,30 @@
 const jwt = require('jsonwebtoken');
-const pool = require('../dbconfig/dbconection');  // Asegúrate de que el pool esté configurado correctamente
+require('dotenv').config();
 
-const verificarToken = (req, res, next) => {
-  const token = req.headers['authorization'];
+// Middleware para verificar el token JWT
+exports.verificarToken = (req, res, next) => {
+  const token = req.header('Authorization');
 
   if (!token) {
-    return res.status(401).json({ message: 'Token requerido' });
+    return res.status(401).json({ message: 'Acceso denegado. Token no proporcionado.' });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-    if (err) {
-      return res.status(403).json({ message: 'Token inválido' });
-    }
-
-    // Obtener el perfil del usuario
-    try {
-      const query = 'SELECT ES_CLI_PERFIL_ID FROM ES_CLIENTE WHERE ES_CLI_ID = $1';
-      const result = await pool.query(query, [decoded.id]);
-
-      if (result.rows.length === 0) {
-        return res.status(404).json({ message: 'Usuario no encontrado' });
-      }
-
-      req.user = decoded;  // Almacenar información del usuario en la solicitud
-      req.perfilId = result.rows[0].es_cli_perfil_id;  // Almacenar el perfil
-
-      next();  // Continuar con la solicitud
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error al verificar el perfil' });
-    }
-  });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.usuario = decoded; // Añadir el usuario decodificado a la solicitud
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Token inválido.' });
+  }
 };
 
-const verificarRol = (rolesPermitidos) => {
-  return (req, res, next) => {
-    if (!rolesPermitidos.includes(req.perfilId)) {
-      return res.status(403).json({ message: 'Acceso denegado' });
-    }
-    next();  // Continuar con la solicitud si tiene el rol adecuado
-  };
-};
+// Middleware para verificar el rol del usuario
+exports.verificarRol = (rolesPermitidos) => (req, res, next) => {
+  const { perfilId } = req.usuario; // Obtener el perfil ID del token decodificado
 
-module.exports = { verificarToken, verificarRol };
+  if (!rolesPermitidos.includes(perfilId)) {
+    return res.status(403).json({ message: 'Acceso denegado. No tienes los permisos necesarios.' });
+  }
+
+  next();
+};
