3fcddfbe44a4424ace0ceb58009e0ad54ae7ea0a
StalinVinueza <csvinueza@puce.edu.ec>
2025-02-11 23:36:33 -0500

    videos

--- FrontEnd/src/App.test.js
+++ FrontEnd/src/App.test.js
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 test('renders learn react link', () => {
   render(<App />);
