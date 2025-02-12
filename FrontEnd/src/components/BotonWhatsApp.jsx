63e42c66bc7b41a8eb0b262fe63f2abe8b4898c6
Pedro <tamayoandres211294@gmail.com>
2025-02-12 16:31:24 -0500

    Contactos

--- FrontEnd/src/components/BotonWhatsApp.jsx
+++ FrontEnd/src/components/BotonWhatsApp.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { FaWhatsapp } from 'react-icons/fa'; 
-import '../components/Contactos.css'; // Asegúrate de que este archivo exista
+import '../components/Contactos.css';
 
 function BotonWhatsApp() {
   return (
     <a
-      href="https://wa.me/593994926633" // Reemplaza con tu número en formato internacional
-      className="btn btn-success whatsapp-btn d-flex align-items-center justify-content-center gap-2"
+      href="https://wa.me/593994926633"
+      className="whatsapp-btn" // Ahora aplicamos la clase de CSS
       target="_blank"
       rel="noopener noreferrer"
     >
-      {/* <FaWhatsapp size={24} />  */}
       Enviar mensaje
     </a>
   );
--- FrontEnd/src/components/Ubicacion.jsx
+++ FrontEnd/src/components/Ubicacion.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
+
 function Ubicacion() {
   return (
     <div className="container mt-4 text-center">
       <h2>Ubicación</h2>
-      <p>Nos encontramos en la Pontificia Universidad Católica del Ecuador (PUCE).</p>
-      
+      <p>Nos encontramos en la Pontificia Universidad Católica del Ecuador (PUCE), un lugar comprometido con el bienestar social y el medio ambiente.</p>
+          
       {/* Mapa de Google embebido */}
       <div className="map-responsive">
         <iframe
           title="Ubicación PUCE"
           width="100%"
-          height="300"
+          height="420"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.555795070853!2d-78.5295425!3d-0.2094901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59a10b8c57123%3A0x7cc4dcd53937a7dd!2sPontificia%20Universidad%20Cat%C3%B3lica%20del%20Ecuador!5e0!3m2!1ses!2sec!4v1675200011225"
           allowFullScreen
         ></iframe>
       </div>
+      
+      <h3>Cómo llegar de manera ecológica</h3>
+      <p>Te invitamos a llegar en transporte sostenible como bicicleta, o en transporte público que minimice el impacto ambiental. Juntos podemos cuidar nuestro planeta. </p>
     </div>
   );
 }
